5b4471ead68132eb7945b928cc1f948cbb0ce4d5
Elliot Wolk <elliot.wolk@gmail.com>
2016-11-28 01:44:02 -0500

    sms_db_importer: dont require an underscore after "PREFIX_<MILLIS>*" when skipping prefix

--- sms_db_importer.py
+++ sms_db_importer.py
@@ -126,7 +126,7 @@ def main():
         for filename in list(mms.attFiles.keys()):
           srcFile = mms.attFiles[filename]
           # prefix any file that doesnt start with PART_<MILLIS>
-          if re.match(r'^PART_\d{13}_', filename):
+          if re.match(r'^PART_\d{13}', filename):
             destFile = args.mms_parts_dir + "/" + filename
           else:
             destFile = args.mms_parts_dir + "/" + attFilePrefix + "_" + filename
